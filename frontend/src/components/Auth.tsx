9c91d57c0baa398e244e18f62b5310163911f5f4
Mohd Danish <danish77355@gmail.com>
2025-02-23 20:37:09 +0530

    update code

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -58,7 +58,7 @@ export const Auth = ({ type }: {type: "signup" | "signin" }) => {
                         <LabelledInput label="Password" placeholder="********" onChange={(e) => {
                             setPostInputs({
                                 ...postInputs,
-                                email: e.target.value
+                                password: e.target.value
                             })
                         }} />
                         <button onClick={sendRequest}  type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
